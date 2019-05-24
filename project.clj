e71c88436a410c5103dfb6fba86e1b9633cb56f5
Euan Cockburn <euan.cockburn@arnoldclark.com>
2019-05-24 11:03:31 +0100

    Generate new clojure project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject rock-paper-scissors-lizard-spock "0.1.0-SNAPSHOT"
+  :description "The game of rock-paper-scissors-lizard-spock"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot rock-paper-scissors-lizard-spock.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
